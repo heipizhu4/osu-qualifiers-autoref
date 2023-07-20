796a00648729afa35461eb6a5e2d8260afb4c6e5
JuanM4T <52723130+JuanM4T@users.noreply.github.com>
2023-07-20 23:38:11 +0200

    Modify original to do qualifier-like lobbies.
    Main changes: 
    - Maps are now picked one after the other, without any user input required. The only thing the user has to do is to invite the players inside the lobby, whether be it by the use of the ">invite" command or by inviting manually within the console. 
    - Scoring is now HeadToHead.
    - There will be multiple output files when running a full lobby: 1 for each player and their score on each map with mapID and 1 for the lobby with a join log.
    - Waiting music is now elevator music. 
    - A lot of spaghetti. Feedback is appreciated.

--- index.js
+++ index.js
@@ -13,19 +13,18 @@ const rl = readline.createInterface({
 const config = require('./config.json');
 const pool = require('./pool.json');
 const match = require('./match.json');
+const { timeStamp, time } = require('console');
+const fechacreacion = new Date();
 
 const client = new bancho.BanchoClient(config);
 const api = new nodesu.Client(config.apiKey);
 
-let channel, lobby;
+let channel, lobby, map, started;
+let i = 0;
+let numplayers = match.teams.length
+console.log(numplayers)
 
-const BLUE = 0, RED = 1;
-match.winningScore = Math.ceil(match.BO/2);
-match.score = [0, 0];
-match.picking = 0;
-
-// turn on to keep track of scores
-// and ask players to pick maps
+//whether to start right away or not
 let auto = false;
 
 // populate mappool with map info
@@ -39,7 +38,7 @@ function initPool() {
 
 // Creates a new multi lobby
 async function init() {
-  console.log(chalk.bold.cyan('Starting osu!autoref'));
+  console.log(chalk.bold.cyan(`id = ${pool[0].id}`));
   await initPool();
   console.log(chalk.bold.green('Loaded map pool!'));
   console.log(chalk.cyan('Attempting to connect...'));
@@ -47,7 +46,9 @@ async function init() {
   try {
     await client.connect();
     console.log(chalk.bold.green("Connected to Bancho!"));
-    channel = await client.createLobby(`${match.tournament}: ${match.teams[BLUE].name} vs ${match.teams[RED].name}`);
+    lobbyname = `${match.tournament}: placeholder`
+    /* lobbyname = `${match.tournament}: ${match.teams[BLUE].name} vs ${match.teams[RED].name}` */
+    channel = await client.createLobby(lobbyname); 
   } catch (err) {
     console.log(err);
     console.log(chalk.bold.red("Failed to create lobby"));
@@ -58,14 +59,15 @@ async function init() {
 
   const password = Math.random().toString(36).substring(8);
   await lobby.setPassword(password);
-  await lobby.setMap(1262832); //hitorigoto dayo
+  await lobby.setMap(975342); //elevator music
 
   console.log(chalk.bold.green("Lobby created!"));
   console.log(chalk.bold.cyan(`Name: ${lobby.name}, password: ${password}`));
   console.log(chalk.bold.cyan(`Multiplayer link: https://osu.ppy.sh/mp/${lobby.id}`));
   console.log(chalk.cyan(`Open in your irc client with "/join #mp_${lobby.id}"`));
+  fs.writeFileSync(`${lobby.id}.txt`, `https://osu.ppy.sh/mp/${lobby.id} | Lobby creada el ${fechacreacion}\n`)
 
-  lobby.setSettings(bancho.BanchoLobbyTeamModes.TeamVs, bancho.BanchoLobbyWinConditions.ScoreV2);
+  lobby.setSettings(bancho.BanchoLobbyTeamModes.HeadToHead, bancho.BanchoLobbyWinConditions.ScoreV2);
 
   createListeners();
 }
@@ -82,7 +84,6 @@ function setBeatmap(input, force=false) {
     const result = pool.filter((map) => {
       return map.name.toLowerCase().includes(input.toLowerCase());
     });
-
     // Prioritize matches to map code before checking by name
     let map;
     if (codeResult.length === 1) {
@@ -99,9 +100,9 @@ function setBeatmap(input, force=false) {
     if (map.mod) {
       mod = map.mod; // if mod explicitly provided (not normal)
     } else if (['HD', 'HR', 'DT'].includes(mapType)) {
-      mod = mapType;
+      mod = mapType + " NF";
     } else if (mapType === 'NM') {
-      mod = 'None';
+      mod = 'NF';
     }
   
     channel.sendMessage("Selecting " + map.name);
@@ -110,75 +111,45 @@ function setBeatmap(input, force=false) {
     return map.code;
   }
 }
-
-function printScore() {
-  channel.sendMessage(`${match.teams[0].name} ${match.score[0]} -- ${match.score[1]} ${match.teams[1].name}`);
-}
-
-function promptPick() {
-  channel.sendMessage(`${match.teams[match.picking].name}, pick the next map`);
-}
-
-// Respond to events occurring in lobby
 function createListeners() {
   lobby.on("playerJoined", (obj) => {
     const name = obj.player.user.username;
-    console.log(chalk.yellow(`Player ${name} has joined!`));
-
-    // Attempt to auto-assign team
-    if (match.teams[BLUE].members.includes(name)) {
-      lobby.changeTeam(obj.player, "Blue");
-    } else if (match.teams[RED].members.includes(name)) {
-      lobby.changeTeam(obj.player, "Red");
-    } else {
-      console.log(chalk.red("Warning! Couldn't figure out team"));
+    console.log(chalk.yellow(`Player ${name} has joined!`))
+    fs.appendFileSync(`${lobby.id}.txt`,`${name} (${Date()})\n`)
+    if(numplayers <= 1){
+      auto = true;
+      if (started){
+        const map = setBeatmap(pool[i].code)
+        if (map) console.log(chalk.cyan(`Changing map to ${map}`))       
+      } else {
+        const map = setBeatmap(pool[0].code)
+        if (map) console.log(chalk.cyan(`Changing map to ${map}`))
+      }
+      started = true;
     }
+    else{
+      numplayers = numplayers - 1;
+      channel.sendMessage(numplayers ? `Welcome. One more left to start.` : `Welcome. There are ${numplayers} players left to join in order to start.`);
+    };
 
-    if (obj.player.user.isClient()) {
-      lobby.setHost("#" + obj.player.user.id);
-    }
-   });
-
+  lobby.on("playerLeft",()=> {
+    numplayers = numplayers + 1;
+    lobby.setMap(975342)
+  })
   lobby.on("allPlayersReady", (obj) => {
     lobby.startMatch(10);
   });
-
-  lobby.on("matchFinished", (scores) => {
-    if (auto) {
-      let s = {"Blue": 0, "Red": 0};
-      scores.forEach((score) => {
-        s[score.player.team] += score.pass * score.score;
-      });
-
-      let diff = s["Blue"] - s["Red"];
-      if (diff > 0) {
-        channel.sendMessage(`${match.teams[BLUE].name} wins by ${diff}`);
-        match.score[BLUE]++;
-      } else if (diff < 0) {
-        channel.sendMessage(`${match.teams[RED].name} wins by ${-diff}`);
-        match.score[RED]++;
-      } else {
-        channel.sendMessage("It was a tie!");
-      }
-
-      match.picking = 1 - match.picking;
-      printScore();
-
-      if (match.score[BLUE] >= match.winningScore) {
-        channel.sendMessage(`${match.teams[BLUE].name} has won the match!`);
-      } else if (match.score[RED] >= match.winningScore) {
-        channel.sendMessage(`${match.teams[RED].name} has won the match!`);
-      } else if (match.score[BLUE] === match.winningScore - 1 && match.score[RED] === match.winningScore - 1) {
-        channel.sendMessage("It's time for the tiebreaker!");
-
-        // bug: after match ends, need to wait a bit before changing map
-        setTimeout(() => setBeatmap('TB', true), 2000);
-      } else {
-        promptPick();
-      }
-    }    
+  lobby.on("matchFinished", (obj) => {
+    obj.forEach(element => {
+      fs.appendFileSync(`${element.player.user.username}.txt`,`${pool[i].code}: ${element.score}\n`);
+      i = i + 1;
+    });
+      if (auto) {
+      const map = setBeatmap(pool[i].code);
+      if (map) console.log(chalk.cyan(`Changing map to ${map}`));
+    }
+   });
   });
-
   channel.on("message", async (msg) => {
     // All ">" commands must be sent by host
     console.log(chalk.dim(`${msg.user.ircUsername}: ${msg.message}`));
@@ -191,42 +162,12 @@ function createListeners() {
           await close();
           break;
         case 'invite':
-          const players = match.teams[0].members.concat(match.teams[1].members);
-          for (const p of players) {
+          for (const p of match.teams) {
             // intentionally fire these synchronously
-            await lobby.invitePlayer(p);
+            await lobby.invitePlayer(p.name);
           }
           break;
-        case 'map':
-          const map = setBeatmap(m.slice(1).join(' '), true);
-          if (map) console.log(chalk.cyan(`Changing map to ${map}`));
-          break;
-        case 'score':
-          match.score[0] = parseInt(m[1]);
-          match.score[1] = parseInt(m[2]);
-          printScore();
-          break;
-        case 'auto':
-          auto = (m[1] === 'on');
-          channel.sendMessage("Auto referee is " + (auto ? "ON" : "OFF"));
-          if (auto) promptPick(); 
-          break;
-        case 'picking':
-          match.picking = (m[1].toLowerCase() === "red" ? 1 : 0);
-          if (auto) promptPick();
-          break;
-        case 'ping':
-          channel.sendMessage("pong");
-          break;
-        default:
-          console.log(chalk.bold.red(`Unrecognized command "${m[0]}"`));
       }
-    } 
-   
-    // people on the picking team can choose just by saying the map name/code
-    if (auto && match.teams[match.picking].members.includes(msg.user.ircUsername)) {
-      const map = setBeatmap(msg.message);
-      if (map) console.log(chalk.cyan(`Changing map to ${map}`));
     }
   });
 }
